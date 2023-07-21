b345bb6dd4f290ed9eb87e58a2b4c8f04b3205bd
yania-pena <yania.pena@udla.edu.ec>
2023-07-20 22:30:12 -0500

    medical-app-backend-1

--- helpers/createJWT.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const createJWT = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
-
-export default createJWT;
--- routes/recordsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import checkAuth from "../middleware/checkAuth.js";
-import { createRecord } from "../controllers/recordsController.js";
-
-const router = express.Router();
-
-router.post("/", checkAuth, createRecord);
-
-export default router;
